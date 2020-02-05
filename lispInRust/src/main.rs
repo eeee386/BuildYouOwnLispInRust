508b3ebe8acbe87b50d29329d4c6fe03808dfe8a
eeee386 <attilasedon1@gmail.com>
2020-02-05 19:52:12 +0100

    Chapter 4 start, wineditline is not working will continue on linux

--- lispInRust/src/main.rs
+++ lispInRust/src/main.rs
@@ -1,3 +1,21 @@
+use std::io::{self, Write};
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input: String = String::new();
+    let args: Vec<String> = env::args().collect();
+    println!("Lispy Version 0.0.0.0.1");
+    println!("Press CTRL+C to exit");
+
+    // In a never ending loop
+    loop {
+        // Output our prompt
+        io::stdout().write_all(b"Lispy>").unwrap();
+        
+        // Read a line of user input of maximum size 2048
+        io::stdin().read_line(&mut input).unwrap();
+        
+        // Echo input back to user input
+        println!("No, you are a {}", input);
+    }
 }
--- lispy.c
+++ lispy.c
@@ -1,3 +1,27 @@
-int main() {
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+#include <editline/history.h>
+
+static char input[2048];
+
+int main(int argc, char** argv){
+    /* Print version and Exit Information */
+    puts("Lispy Version 0.0.0.0.1");
+    puts("Press Ctrl+c Exit\n");
+
+    // In a never ending loop
+    while(1){
+        // Output our prompt
+        fputs("lispy>", stdout);
+        
+        // Read a line of user input of maximum size 2048
+        fgets(input, 2048, stdin);
+
+        // Echo input back to user input
+        printf("No you're a %s", input);
+    }
+
     return 0;
 }
